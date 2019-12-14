87147072c820fde9ba38b41a058dba574adcbbcf
Zofia Natanek <zofia.natanek@gmail.com>
2019-12-14 17:40:36 +0100

    Add fridge routes

---
+++ routes/fridge.js
@@ -0,0 +1,10 @@
+const _ = require('lodash');
+// const { FridgeContent, addToFridge } = require('../models/fridge');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -7,7 +7,7 @@ const { Unit } = require('../models/unit');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async (req, res) => {
+router.post('/', async (req, res) => {
     const { error } = validateProduct(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     let product = await Product.findOne({ name: req.body.name });
@@ -15,13 +15,13 @@ router.post('/', auth, async (req, res) => {
     addProduct(req.body, res);
 })
 
-router.get('/:product', auth, async (req, res) => {
+router.get('/:product', async (req, res) => {
     let product = !isNaN(parseInt(req.params.product)) ? await Product.findOne({ id: req.params.product }).populate('units', 'name -_id').select('id name type units -_id') : await Product.findOne({ name: req.params.product }).populate('units', 'name -_id').select('id name type units -_id');
     if (product) return res.send(product);
     return res.status(404).send(`That product doesn't exist`);
 })
 
-router.get('/', auth, async (req, res) => {
+router.get('/', async (req, res) => {
     const products = await Product.find().populate('units', 'name -_id').select('id name type units -_id');
     return res.send(products);
 })
