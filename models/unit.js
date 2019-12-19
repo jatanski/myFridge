eac9671adc36d7eb9d2431bf66560316d0114d70
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-12-19 18:58:30 +0100

    basic functionality

--- models/unit.js
+++ models/unit.js
@@ -1,9 +1,43 @@
 const mongoose = require('mongoose');
 
-const Unit = mongoose.model('Unit', new mongoose.Schema({
-    name: String,
-    type: String,
-    value: Number
-}));
+const Unit = mongoose.model(
+    'Unit',
+    new mongoose.Schema({
+        name: String,
+        type: String,
+        value: Number,
+    }),
+);
+
+async function getUnits() {
+    try {
+        let units = await Unit.find();
+        return units;
+    } catch (error) {
+        return error;
+    }
+}
+
+async function getValue(unitName, amount) {
+    let result = await Unit.findOne({ name: unitName });
+    return !!result ? result.value * amount : 0;
+}
+
+async function sumValues(products) {
+    let sum = 0;
+    let error = false;
+    for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+        value = await getValue(product.unit, product.amount);
+        if (!value) {
+            error = true;
+        }
+        sum += value;
+    }
+    return error ? { sum, error } : sum;
+}
 
 exports.Unit = Unit;
+exports.getUnits = getUnits;
+exports.getValue = getValue;
+exports.sumValues = sumValues;
--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const { ShoppingListItem, validateItem, addShoppingListItem } = require('../models/shoppingListItem');
-const { Unit } = require('../models/unit');
+const { Unit, getUnits, getValue, sumValues } = require('../models/unit');
 
 router.post('/', auth, async (req, res) => {
     const { error } = validateItem(req.body);
@@ -11,50 +11,76 @@ router.post('/', auth, async (req, res) => {
     let item = await ShoppingListItem.findOne({ name: req.body.name });
     if (item) return res.status(400).send('that product already exist');
     addShoppingListItem(req.body, res);
-})
+});
 
-router.get('/:shoppingListItem', auth, async (req, res) => {
-    let item = !isNaN(parseInt(req.params.shoppingListItem)) 
+router.get('/:shoppingListItem', async (req, res) => {
+    console.log(req.params.shoppingListItem);
+    const value = await getValue(req.params.shoppingListItem, 100);
+    console.log('value', value);
+    const sum = await sumValues([
+        { unit: 'cup', amount: 1 },
+        { unit: 'cup', amount: 1 },
+        { unit: 'mililiter', amount: 100 },
+    ]);
+    console.log('sum', sum);
+    let item = !isNaN(parseInt(req.params.shoppingListItem))
         ? await ShoppingListItem.findOne({ id: req.params.shoppingListItem })
-            .populate('units', 'name -_id').select('id name quantity units -_id')
+              .populate('units', 'name -_id')
+              .select('id name quantity units -_id')
         : await ShoppingListItem.findOne({ name: req.params.shoppingListItem })
-            .populate('units', 'name -_id').select('id name quantity units -_id');
+              .populate('units', 'name -_id')
+              .select('id name quantity units -_id');
     if (item) return res.send(item);
     return res.status(404).send(`That product doesn't exist`);
-})
+});
 
-router.get('/', auth, async (req, res) => {
-    const items = await ShoppingListItem.find().populate('units', 'name -_id').select('id name quantity units -_id');
+router.get('/', async (req, res) => {
+    console.log(await getUnits());
+    const items = await ShoppingListItem.find()
+        .populate('units', 'name -_id')
+        .select('id name quantity units -_id');
     return res.send(items);
-})
+});
 
-router.put('/:shoppingListItem', auth, async (req, res) => {    
-    let item = !isNaN(parseInt(req.params.shoppingListItem)) 
+router.put('/:shoppingListItem', auth, async (req, res) => {
+    let item = !isNaN(parseInt(req.params.shoppingListItem))
         ? await ShoppingListItem.findOne({ id: req.params.shoppingListItem })
         : await ShoppingListItem.findOne({ name: req.params.shoppingListItem });
 
-    item = !isNaN(parseInt(req.params.shoppingListItem)) 
-    ? await ShoppingListItem.findOneAndUpdate({ id: req.params.shoppingListItem }, {
-        $set:{
-            id: (!!req.body.id)? req.body.id : item.id,
-            name: (!!req.body.name)? req.body.name : item.name,
-            units: (!!req.body.units)? req.body.units : item.units,
-            quantity: (!!req.body.quantity)? req.body.quantity : item.quantity}
-        }, {returnNewDocument : true, upsert: true, new: true})
-    : await ShoppingListItem.findOneAndUpdate({ name: req.params.shoppingListItem }, {
-        $set:{
-            id: (!!req.body.id)? req.body.id : item.id,
-            name: (!!req.body.name)? req.body.name : item.name,
-            units: (!!req.body.units)? req.body.units : item.units,
-            quantity: (!!req.body.quantity)? req.body.quantity : item.quantity}
-        }, {returnNewDocument : true, upsert: true, new: true});
+    item = !isNaN(parseInt(req.params.shoppingListItem))
+        ? await ShoppingListItem.findOneAndUpdate(
+              { id: req.params.shoppingListItem },
+              {
+                  $set: {
+                      id: !!req.body.id ? req.body.id : item.id,
+                      name: !!req.body.name ? req.body.name : item.name,
+                      units: !!req.body.units ? req.body.units : item.units,
+                      quantity: !!req.body.quantity ? req.body.quantity : item.quantity,
+                  },
+              },
+              { returnNewDocument: true, upsert: true, new: true },
+          )
+        : await ShoppingListItem.findOneAndUpdate(
+              { name: req.params.shoppingListItem },
+              {
+                  $set: {
+                      id: !!req.body.id ? req.body.id : item.id,
+                      name: !!req.body.name ? req.body.name : item.name,
+                      units: !!req.body.units ? req.body.units : item.units,
+                      quantity: !!req.body.quantity ? req.body.quantity : item.quantity,
+                  },
+              },
+              { returnNewDocument: true, upsert: true, new: true },
+          );
     return res.send(item);
-})
+});
 
 router.delete('/:shoppingListItem', auth, async (req, res) => {
-    return res.send(!isNaN(parseInt(req.params.shoppingListItem)) 
-        ? await ShoppingListItem.deleteOne({ id: req.params.shoppingListItem })
-        : await ShoppingListItem.deleteOne({ name: req.params.shoppingListItem }));
-})
+    return res.send(
+        !isNaN(parseInt(req.params.shoppingListItem))
+            ? await ShoppingListItem.deleteOne({ id: req.params.shoppingListItem })
+            : await ShoppingListItem.deleteOne({ name: req.params.shoppingListItem }),
+    );
+});
 
 module.exports = router;
