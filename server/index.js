36f10b969f7213e5f2f2aadb8034f80ad7da4ed8
Kuba <ja.tanski@gmail.com>
2019-12-19 21:11:25 +0100

    fix recipe

--- server/index.js
+++ server/index.js
@@ -1,26 +1,23 @@
 const config = require('config');
-const express = require("express");
+const express = require('express');
 const app = express();
 
-require("./startup/prod")(app);
-require("./startup/db")();
+require('./startup/prod')(app);
+require('./startup/db')();
 require('./startup/routes');
 
 app.use(express.json());
 
-
 if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1);
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get('/', (req, res) => {
+    res.send('Hello world');
 });
 
 const port = process.env.PORT || 8000;
-const index = app.listen(port, () =>
-  console.log(`Listening on port ${port}...`)
-);
+const index = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = index;
--- server/startup/routes.js
+++ server/startup/routes.js
@@ -5,13 +5,15 @@ const auth = require('../routes/auth');
 const products = require('../routes/products');
 const shoppingList = require('../routes/shoppingList');
 const fridge = require('../routes/fridge');
+const recipe = require('../routes/recipes');
 
-module.exports = function (app) {
-  app.use(express.json());
-  app.use(error);
-  app.use('/api/users', users);
-  app.use('/api/auth', auth);
-  app.use('/api/products', products);
-  app.use('/api/shoppingList', shoppingList);
-  app.use('/api/fridge', fridge);
+module.exports = function(app) {
+    app.use(express.json());
+    app.use(error);
+    app.use('/api/users', users);
+    app.use('/api/auth', auth);
+    app.use('/api/products', products);
+    app.use('/api/shoppingList', shoppingList);
+    app.use('/api/fridge', fridge);
+    app.use('./api/recipe', recipe);
 };
