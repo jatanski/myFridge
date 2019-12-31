9315e43dd7737efe2ec6e13b9dd0c08ed83304b7
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-12-31 16:56:21 +0100

    add nav to every page

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import ExamplePage from '../pages/ExamplePage';
+import Navigation from '../components/Navigation';
 
 export default function AppRouter() {
   return (
     <Router>
+      <Navigation />
       <Route path="/" exact component={Home} />
       <Route path="/example" component={ExamplePage} />
     </Router>
