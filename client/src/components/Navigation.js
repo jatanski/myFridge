934d90946523b3aa0cb0be1b75e0b7f84609bbe3
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-12-31 17:40:46 +0100

    improve looks

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { MDBNav, MDBNavItem, MDBNavLink } from 'mdbreact';
+import { MDBNav, MDBNavLink, MDBIcon } from 'mdbreact';
 import '../scss/components/Navigation.scss';
 
 export default function Navigation() {
   return (
-    <MDBNav>
-      <MDBNavLink to="/">Logo</MDBNavLink>
-      <MDBNavLink to="/example">READ ME!</MDBNavLink>
-      <MDBNavLink to="/example">Example</MDBNavLink>
+    <MDBNav color="grey">
+      <MDBNavLink className="white-text" to="/">
+        <MDBIcon icon="home" />
+      </MDBNavLink>
+      <MDBNavLink className="white-text" to="/example">
+        READ ME!
+      </MDBNavLink>
+      <MDBNavLink className="white-text" to="/example">
+        Example
+      </MDBNavLink>
     </MDBNav>
   );
 }
