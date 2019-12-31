d78b0a6a44f133c78af96933e47f65bdb1771f7e
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-12-31 17:11:12 +0100

    add mock nav + styles

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../scss/components/Navigation.scss';
 
 export default function Navigation() {
   return (
     <nav>
-      <Link to="/">
+      <Link to="/" className="link">
         <h4>Home</h4>
       </Link>
-      <Link to="/example">
-        <h4>Example</h4>
-      </Link>
+      <ul>
+        <Link to="/example" className="link">
+          <li>Example</li>
+        </Link>
+        <Link to="/example" className="link">
+          <li>Example</li>
+        </Link>
+      </ul>
     </nav>
   );
 }
