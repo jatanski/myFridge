9abb2cfd4cae0b41f6ca39dfbedbb9deb9a41997
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-12-31 16:49:47 +0100

    remove stuff, add basic router

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/global.scss';
-import App from './pages/App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Router from './router/router';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<Router />, document.getElementById('root'));
---
+++ client/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../scss/pages/app.scss';
+
+function Home() {
+  return <div className="App">Its working</div>;
+}
+
+export default Home;
