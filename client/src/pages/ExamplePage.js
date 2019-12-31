ae24ffb8673410ed912ca28695a16eb0dfa8f3f8
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-12-31 16:52:26 +0100

    add example page

---
+++ client/src/pages/ExamplePage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ExamplePage() {
+  return <div>this is example page</div>;
+}
--- client/src/router/router.js
+++ client/src/router/router.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import ExamplePage from '../pages/ExamplePage';
 
 export default function AppRouter() {
   return (
     <Router>
-      <Route path="/" component={Home} />
+      <Route path="/" exact component={Home} />
+      <Route path="/example" component={ExamplePage} />
     </Router>
   );
 }
