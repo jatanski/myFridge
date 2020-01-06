d3a8f955a2ed063f75ec8f002d061e804c35532b
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2020-01-06 22:04:30 +0100

    allow disabling certain elements and auto loading data

--- client/src/components/fridge/FridgeContent.js
+++ client/src/components/fridge/FridgeContent.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import axios from 'axios';
-import { MDBBtn } from "mdbreact";
-import ItemsDisplay from './ItemsDisplay'
-import AddItem from './Items/AddItem'
+import { MDBBtn } from 'mdbreact';
+import ItemsDisplay from './ItemsDisplay';
+import AddItem from './Items/AddItem';
 
 class FridgeContent extends React.Component {
-    state = { items: [] };
+  state = { items: [] };
 
-    getFridgeContent = async (items) => {
-        const response = await axios.get('http://localhost:8000/api/fridge', {
-
-        });
-        this.setState({ items: response.data });
-        console.log(this.state.items)    
+  componentDidMount() {
+    if (this.props.autoLoad) {
+      this.getFridgeContent();
     }
+  }
 
-    render() {
-        return (
-            <div>
-            <MDBBtn outline onClick={this.getFridgeContent}>Show content</MDBBtn>
-            <ItemsDisplay items ={this.state.items}/>
-            <AddItem/>
-            </div>
-        )
-    }
+  getFridgeContent = async items => {
+    const response = await axios.get('http://localhost:8000/api/fridge', {});
+    this.setState({ items: response.data });
+    console.log(this.state.items);
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.autoLoad ? null : (
+          <MDBBtn outline onClick={this.getFridgeContent}>
+            Show content
+          </MDBBtn>
+        )}
+        <ItemsDisplay items={this.state.items} disableRemoving={this.props.disableRemoving} />
+        {this.props.disableAdding ? null : <AddItem />}
+      </div>
+    );
+  }
 }
 
-export default FridgeContent
+export default FridgeContent;
--- client/src/components/fridge/ItemsDisplay.js
+++ client/src/components/fridge/ItemsDisplay.js
@@ -1,42 +1,41 @@
 import React from 'react';
 import { MDBContainer, MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
-import RemoveItem from './Items/RemoveItem'
+import RemoveItem from './Items/RemoveItem';
 
-
-
-const ItemsDisplay = (props) => {
-    const items = props.items.map((item, index) => {
-        return (
-            <tr id='item-column' >
-                <td>{index + 1}</td>
-                <td>{item.product.name}</td>
-                <td>{item.units}</td>
-                <td>{item.avaliableQuantity}</td>
-                <td value = {item._id}><RemoveItem/></td>
-            </tr>
-            )
-    })
+const ItemsDisplay = props => {
+  const items = props.items.map((item, index) => {
     return (
-        <MDBContainer className="mt-5 text-center">
-            <MDBTable bordered>
-                <MDBTableHead>
-                    <tr>
-                        <th>Index</th>
-                        <th>Item</th>
-                        <th>Units</th>
-                        <th>Quantity</th>
-                        <th>Actions</th>
-                    </tr>
-                </MDBTableHead>
-                <MDBTableBody>
-                   {items}
-
-                </MDBTableBody>
-            </MDBTable>
-
-        </MDBContainer>
-    )
-    //<div>{items}</div>;
+      <tr id="item-column">
+        <td>{index + 1}</td>
+        <td>{item.product.name}</td>
+        <td>{item.units}</td>
+        <td>{item.avaliableQuantity}</td>
+        {props.disableRemoving ? null : (
+          <td value={item._id}>
+            <RemoveItem />
+          </td>
+        )}
+      </tr>
+    );
+  });
+  console.log(props);
+  return (
+    <MDBContainer className="mt-5 text-center">
+      <MDBTable bordered>
+        <MDBTableHead>
+          <tr>
+            <th>Index</th>
+            <th>Item</th>
+            <th>Units</th>
+            <th>Quantity</th>
+            {props.disableRemoving ? null : <th>Actions</th>}
+          </tr>
+        </MDBTableHead>
+        <MDBTableBody>{items}</MDBTableBody>
+      </MDBTable>
+    </MDBContainer>
+  );
+  //<div>{items}</div>;
 };
 
 export default ItemsDisplay;
