9d0eb63fda5a487c757e3321af6f7078d087ff10
Arek Moszczynski <arekmoszczynski@gmail.com>
2020-01-06 17:34:18 +0100

    podpiete zewnetrzne API

---
+++ client/src/pages/MealListPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MealListComp from '../components/MealListComp';
+
+
+function MealListPage() {
+    return <MealListComp />;
+}
+
+export default MealListPage;
+
+
--- client/src/router/router.js
+++ client/src/router/router.js
@@ -6,6 +6,7 @@ import ExamplePage from '../pages/ExamplePage';
 import Navigation from '../components/Navigation';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import MealListPage from '../pages/MealListPage';
 
 export default function AppRouter() {
   return (
@@ -15,6 +16,7 @@ export default function AppRouter() {
       <Route path="/example" component={ExamplePage} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/mealList" component={MealListPage} />
     </Router>
   );
 }
