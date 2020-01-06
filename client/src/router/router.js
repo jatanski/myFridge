3ebbfca35d0f6a43191248031da6e4fe9a7abe0a
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2020-01-06 23:09:56 +0100

    allow hiding navigation

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import Home from '../pages/Home';
 import Navigation from '../components/Navigation';
@@ -8,9 +9,10 @@ import Register from '../pages/Register';
 import Fridge from '../pages/Fridge';
 
 export default function AppRouter() {
+  const history = createBrowserHistory();
   return (
     <Router>
-      <Navigation />
+      {history.location.pathname !== '/wyjatek' ? <Navigation /> : null}
       <Route path="/" exact component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
