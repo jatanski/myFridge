e09bc6eaff6eaf9b60111086bb7c79d4264526f9
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2020-01-06 22:04:44 +0100

    add dashboard

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import '../scss/pages/app.scss';
+import Dashboard from '../components/Dashboard';
 
 function Home() {
-  return <div className="App">Its working</div>;
+  return (
+    <div className="App">
+      <Dashboard></Dashboard>
+    </div>
+  );
 }
 
 export default Home;
