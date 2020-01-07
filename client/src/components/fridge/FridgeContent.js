c4fafc06783c2ec098ce9a2bf79c6559fc9e829f
juriakajurek <eldiareq@gmail.com>
2020-01-07 02:57:38 +0100

    fix bug

--- client/src/components/fridge/FridgeContent.js
+++ client/src/components/fridge/FridgeContent.js
@@ -13,11 +13,6 @@ class FridgeContent extends React.Component {
     }
   }
 
-    displayForm = () => {
-        window.open('AddItemToDB', '_blank',
-         'width=600,height=700,left=200,top=200,location=no,manubar=no,status=no,titlebar=no,toolbar=no');
-  }
-
   getFridgeContent = async items => {
     const response = await axios.get('http://localhost:8000/api/fridge', {});
     this.setState({ items: response.data });
@@ -34,9 +29,6 @@ class FridgeContent extends React.Component {
         )}
         <ItemsDisplay items={this.state.items} disableRemoving={this.props.disableRemoving} />
         {this.props.disableAdding ? null : <AddItem />}
-        <div style={{display: "flex", flexDirection: 'column' }} >
-            <MDBBtn to="/example" onClick={this.displayForm} outline style={{ alignSelf: "flex-end" }} size="sm" color="blue-grey" >Add new product to database</MDBBtn>
-        </div>
       </div>
     );
   }
--- client/src/pages/Fridge.js
+++ client/src/pages/Fridge.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../scss/pages/app.scss';
-import { MDBContainer } from 'mdbreact';
+import { MDBContainer, MDBBtn } from 'mdbreact';
 import FridgeContent from '../components/fridge/FridgeContent'
 
+const displayForm = () => {
+  window.open('AddItemToDB', '_blank',
+   'width=600,height=700,left=200,top=200,location=no,manubar=no,status=no,titlebar=no,toolbar=no');
+}
 
 function Fridge() {
   return (
       <MDBContainer className="mt-5 text-center">
         <h4>Welcome to your fridge</h4>
         <FridgeContent/>
+        <br />
+        <div style={{display: "flex", flexDirection: 'column' }} >
+            <MDBBtn onClick={displayForm} outline style={{ alignSelf: "flex-end" }} size="sm" color="blue-grey" >Add new product to database</MDBBtn>
+        </div>
       </MDBContainer>
       );
 }
